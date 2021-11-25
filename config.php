176eb22cb54c8bb05273aa6f6fd57cb04a9b0a84
AntonViktorovichD <white.trash.without.cash@gmail.com>
2021-11-25 15:43:13 +0300

    Sqlparser

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define("HOST",'localhost');
+define("USER",'root');
+define("PASSWORD",'');
+define("DB",'test');
+
+define("DIR_SQL",'sql/');
+define("FOR_WRITE",10);
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-Type:text/html;charset=UTF-8");
+
+include "config.php";
+include "functions.php";
+
+$db = connect_db();
+
+$tables = get_tables($db);
+
+get_dump($db,$tables);
+
+
+var_dump($tables);
+
+?>
